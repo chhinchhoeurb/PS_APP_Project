2636cb5fb818c1f47a4bd78de6edde862aa5c60e
sreymompnc <sreymom.mann@gmail.com>
2017-08-30 13:04:26 +0700

    home page and post detail

--- app/src/main/java/ruc/ps_app_project/HomeAdapter.java
+++ app/src/main/java/ruc/ps_app_project/HomeAdapter.java
@@ -5,25 +5,41 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
 
 public class HomeAdapter extends ArrayAdapter {
 
     Context context;
-    String countryList[];
-    int myflag[];
+    List<String> username,dateAndTime,description,profile, allPostImage,numLikes,numFav,numCmt;
 
-    public HomeAdapter(Context applicationContext, String[] countryList, int[] flags) {
+    public HomeAdapter(Context applicationContext, List<String> username,List<String> dateAndTime,
+                       List<String> description,List<String> profile, List<String> allPostImage,
+                       List<String> numLikes,List<String> numFav,List<String> numCmt) {
         super(applicationContext,R.layout.homelist_item);
         this.context = applicationContext;
-        this.countryList = countryList;
-        this.myflag = flags;
+        this.username = username;
+        this.dateAndTime = dateAndTime;
+        this.description = description;
+        this.profile = profile;
+        this.allPostImage = allPostImage;
+        this.numLikes = numLikes;
+        this.numFav = numFav;
+        this.numCmt = numCmt;
     }
+
+
+
 
     @Override
     public int getCount() {
-        return countryList.length;
+        return username.size();
     }
 
     @Override
@@ -45,12 +61,20 @@ public class HomeAdapter extends ArrayAdapter {
         if (Listview == null){
 
             LayoutInflater mInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-            Listview = mInflater.inflate(R.layout.listview_item, parent, false);
+            Listview = mInflater.inflate(R.layout.homelist_item, parent, false);
 
             holder = new ViewHolder();
 
-            holder.images = (ImageView) Listview.findViewById(R.id.displayImage);
+            holder.createDate = (TextView)Listview.findViewById(R.id.datetime) ;
             holder.usernames = (TextView) Listview.findViewById(R.id.userItem);
+            holder.desc = (TextView)Listview.findViewById(R.id.descrip) ;
+            holder.posterProfile = (ImageView)Listview.findViewById(R.id.circle_image) ;
+            holder.postImages = (ImageView) Listview.findViewById(R.id.displayImage);
+
+            holder.btnLike = (Button)Listview.findViewById(R.id.btnlike) ;
+            holder.btnFav = (Button)Listview.findViewById(R.id.btnfavorite) ;
+            holder.bntCmt = (Button)Listview.findViewById(R.id.btncmt) ;
+
 
 
             Listview.setTag(holder);
@@ -60,16 +84,57 @@ public class HomeAdapter extends ArrayAdapter {
         }
 
 
-        holder.usernames.setText(countryList[position]);
-        holder.images.setImageResource(myflag[position]);
+        holder.usernames.setText(username.get(position));
+        holder.createDate.setText(dateAndTime.get(position));
+        holder.desc.setText(description.get(position));
+        holder.btnLike.setText(numLikes.get(position));
+        holder.btnFav.setText(numFav.get(position));
+        holder.bntCmt.setText(numCmt.get(position));
+        // profile
+        final String url = "http://192.168.1.14:1111/images/posters/"+profile.get(position);
+        loadImage(url,holder.posterProfile );
+        // post image
+        final String postImageurl = "http://192.168.1.14:1111/images/posts/"+allPostImage.get(position);
+        loadImagePost(postImageurl,holder.postImages);
+
 
 
         return Listview;
+
+
     }
 
 
     public static class ViewHolder {
-        ImageView images;
         TextView usernames;
+        TextView createDate;
+        TextView desc;
+        ImageView posterProfile;
+        ImageView postImages;
+        Button btnLike,bntCmt,btnFav;
     }
+
+    // To load image of profile
+    private void loadImage(String url,ImageView imgView){
+        Picasso.with(context)
+                .load(url)
+                .resize(800,800)
+                .centerInside()// to zoom img
+                //.centerCrop()
+                .into(imgView);
+
+    }
+
+    // To load image of post
+    private void loadImagePost(String url,ImageView imgView){
+        Picasso.with(context)
+                .load(url)
+                .resize(1300,1200)
+                .centerInside()// to zoom img
+                //.centerCrop()
+                .into(imgView);
+
+    }
+
+
 }
